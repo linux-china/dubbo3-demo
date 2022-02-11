16b6d9ede4f36bdf2bc2e18ac2bc02837c4277f3
linux_china <libing.chen@gmail.com>
2022-02-11 12:08:12 -0800

    Account

---
+++ src/main/java/org/mvnsearch/Account.java
@@ -0,0 +1,33 @@
+package org.mvnsearch;
+
+
+import java.io.Serializable;
+
+public class Account implements Serializable {
+    private Integer id;
+    private String email;
+
+    public Account() {
+    }
+
+    public Account(Integer id, String email) {
+        this.id = id;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
